9d20bacdb92e0b010daef56b593564b3b0b7252e
unknown <lucas.marais.wrkgmail.con>
2023-05-03 10:46:19 +0200

    Starting of the project

---
+++ src/backend/Simulator.java
@@ -0,0 +1,206 @@
+package backend;
+
+
+import windowInterface.MyInterface;
+//TODO : add imports you will need here
+/*
+ *  Note : if you use an import in another class you will need to add
+ *  the import lines on top of the file of the other class.
+ */
+// Examples of useful imports :
+// import java.util.LinkedList;
+// import java.util.ArrayList;
+// import java.util.Random;
+
+public class Simulator extends Thread {
+
+	private MyInterface mjf;
+	private boolean stopFlag;
+	private boolean pauseFlag;
+	private int loopDelay;
+	//TODO : add declaration of additional attributes here
+
+	public Simulator(MyInterface mjfParam) {
+		mjf = mjfParam;
+		stopFlag=false;
+		pauseFlag=false;
+		loopDelay = 150;
+		//TODO : add other attribute initialization here
+
+	}
+	
+	/**
+	 * getter of the width of the simulated world
+	 * @return the number of columns in the grid composing the simulated world
+	 */
+	public int getWidth() {
+		//TODO : correct return
+		return 0;
+	}
+
+	/**
+	 * getter of the height of the simulated world
+	 * @return the number of rows in the grid composing the simulated world
+	 */
+	public int getHeight() {
+		//TODO : correct return
+		return 0;
+	}
+	
+	public void run() {
+		//WARNING : Do not modify this.
+		/*Exception : 
+		 *if everything you have to do works and you have a backup... 
+		 *have fun editing this if you want to enhance it!
+		 *But be sure to take into account that this class inherits from Thread
+		 *You might want to check documentation online about the Thread class
+		 *But do not hesitate to email me any questions 
+		*/
+		int stepCount=0;
+		while(!stopFlag) {
+			stepCount++;
+			makeStep();
+			mjf.update(stepCount);
+			try {
+				Thread.sleep(loopDelay);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			while(pauseFlag && !stopFlag) {
+				try {
+					Thread.sleep(loopDelay);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+
+	}
+
+	/**
+	 * Individual step of the Simulation, modifying the world from
+	 * its state at time t to its state at time t+1
+	 */
+	public void makeStep() {
+		//TODO : fill in for Simulator behavior.
+		/*
+		 * Do not Hesitate to write other (private) methods in this class 
+		 * to use them here,
+		 * or other classes from which you might use instances here, 
+		 * be it as variables or as attributes you may add to the class Simulator,
+		 * by using their (public) methods.
+		 */
+		
+	}
+
+	/**
+	 * Stops simulation by raising the stop flag used in the run method
+	 */
+	public void stopSimu() {
+		//TODO : set stopFlag to true
+		
+	}
+
+	/**
+	 * Toggles Pause of simulation 
+	 * by raising or lowering the pause flag used in the run method
+	 */
+	public void togglePause() {
+		//TODO : change value of boolean attribute pauseFlag
+		// from false to true, or from true to false
+
+	}
+	/**
+	 * Changes content value of the Cell at the coordinates specified in arguments
+	 * 
+	 * 
+	 * @param x coordinate on the x-axis (horizontal)
+	 * @param y coordinate on the y-axis (vertical)
+	 */
+	public void toggleCell(int x, int y) {
+		//TODO : change the value of the cell at coordinates (x,y)
+		/*
+		 * Note : the value of the cell is NOT a boolean, it is an integer.
+		 * O means dead, 1 means alive...
+		 * But the GUI can also print properly more values than that.
+		 * You might want to use this for the going further section.
+		 */
+	}
+	/**
+	 * get the value of a cell at coordinates
+	 * @param x coordinate
+	 * @param y coordinate
+	 * @return the value of the cell
+	 */
+	public int getCell(int x, int y) {
+		//TODO implement proper return
+		return 0;
+	}
+
+	/**
+	 * set the value of a cell at coordinates
+	 * @param x coordinate
+	 * @param y coordinate
+	 * @param val the value to set inside the cell
+	 */
+	public void setCell(int x, int y, int val) {
+		//TODO implement
+	}
+
+	/**
+	 * Each String in the returned array represents a [row/column]
+	 * 
+	 * @return an array of Strings representing the simulated world's state
+	 */
+	public String[] getFileRepresentation() {
+		//TODO : implement
+		return new String[0];
+	}
+	/**
+	 * Populates a [row/column] indicated by the given coordinate
+	 * using its String representation
+	 * 
+	 * @param y the y coordinate of the row/column to populate
+	 * @param fileLine the String line representing the row
+	 */
+	public void populateLine(int coord, String fileLine) {
+		//TODO : implement and correct the comment
+		// As you have to choose row OR column depending on your implementation
+	}
+	
+	/**
+	 * populates world with randomly living cells
+	 * 
+	 * @param chanceOfLife the probability, expressed between 0 and 1, 
+	 * that any given cell will be living
+	 */
+	public void generateRandom(float chanceOfLife) {
+		//TODO implement
+	}
+	
+	/**
+	 * Checks if the borders are looping
+	 * 
+	 * @return true if the borders are looping, false otherwise
+	 */
+	public boolean isLoopingBorder() {
+		//TODO implement correct return
+		return false;
+	}
+	
+	/**
+	 * Toggle the looping of borders, activating or deactivating it
+	 * depending on the present state
+	 */
+	public void toggleLoopingBorder() {
+		//TODO implement
+	}
+	
+	/**
+	 * Setter for the delay between steps of the simulation
+	 * @param delay in milliseconds
+	 */
+	public void setLoopDelay(int delay) {
+		//TODO : implement
+	}
+}
---
+++ src/windowInterface/MyInterface.java
@@ -0,0 +1,302 @@
+package windowInterface;
+
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.GridLayout;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JSlider;
+import javax.swing.SwingConstants;
+import javax.swing.border.EmptyBorder;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+
+import backend.Simulator;
+
+import javax.swing.JButton;
+import javax.swing.JFileChooser;
+import javax.swing.JLabel;
+
+import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.awt.event.ActionEvent;
+
+public class MyInterface extends JFrame {
+
+	private static final long serialVersionUID = -6840815447618468846L;
+	private JPanel contentPane;
+	private JLabel stepLabel;
+	private JLabel borderLabel;
+	private JLabel speedLabel;
+	private JPanelDraw panelDraw;
+	private Simulator mySimu = null;
+	private JSlider randSlider;
+	private JSlider speedSlider;
+
+	/**
+	 * Create the frame.
+	 */
+	public MyInterface() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(10, 10, 500, 400);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+
+		JPanel panelTop = new JPanel();
+		contentPane.add(panelTop, BorderLayout.NORTH);
+
+		JPanel panelRight = new JPanel();
+		panelRight.setLayout(new GridLayout(10, 1));
+		contentPane.add(panelRight, BorderLayout.EAST);
+
+		JButton btnGo = new JButton("Start/Pause");
+		btnGo.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				clicButtonGo();
+			}
+		});
+		panelTop.add(btnGo);
+
+//		JButton btnPause = new JButton("Toggle Pause");
+//		btnPause.addActionListener(new ActionListener() {
+//			public void actionPerformed(ActionEvent arg0) {
+//				clicButtonPause();
+//			}
+//		});
+//		panelTop.add(btnPause);
+
+		stepLabel = new JLabel("Step : X");
+		panelTop.add(stepLabel);
+
+		JButton btnStop = new JButton("Stop");
+		btnStop.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				clicButtonStop();
+			}
+		});
+		panelTop.add(btnStop);
+
+		speedLabel = new JLabel("speed slider : ");
+		panelTop.add(speedLabel);
+
+		speedSlider = new JSlider();
+		speedSlider.setValue(3);
+		speedSlider.setMinimum(0);
+		speedSlider.setMaximum(10);
+		speedSlider.setOrientation(SwingConstants.HORIZONTAL);
+		speedSlider.setPreferredSize(new Dimension(100, 30));
+		speedSlider.addChangeListener(new ChangeListener() {
+			public void stateChanged(ChangeEvent arg0) {
+				changeSpeed();
+			}
+		});
+		panelTop.add(speedSlider);
+
+//		JButton btnSpeed = new JButton("Set Speed");
+//		btnSpeed.addActionListener(new ActionListener() {
+//			public void actionPerformed(ActionEvent e) {
+//				clicButtonSpeed();
+//			}
+//		});
+//		panelTop.add(btnSpeed);
+
+		JButton btnLoad = new JButton("Load File");
+		btnLoad.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				clicLoadFileButton();
+			}
+		});
+		panelRight.add(btnLoad);
+
+		JButton btnSave = new JButton("Save To File");
+		btnSave.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				clicSaveToFileButton();
+			}
+		});
+		panelRight.add(btnSave);
+
+		JButton btnRandGen = new JButton("Random");
+		btnRandGen.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				generateRandomBoard();
+			}
+		});
+		panelRight.add(btnRandGen);
+
+		JLabel randLabel = new JLabel("random density slider :");
+		panelRight.add(randLabel);
+
+		randSlider = new JSlider();
+		randSlider.setValue(50);
+		randSlider.setMinimum(0);
+		randSlider.setMaximum(100);
+		randSlider.setPreferredSize(new Dimension(30, 200));
+		panelRight.add(randSlider);
+
+		JButton btnBorder = new JButton("Toggle Border");
+		btnBorder.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				clicButtonBorder();
+			}
+		});
+		panelRight.add(btnBorder);
+
+		borderLabel = new JLabel("border : X");
+		panelRight.add(borderLabel);
+
+		panelDraw = new JPanelDraw(this);
+		contentPane.add(panelDraw, BorderLayout.CENTER);
+	}
+
+	public void setStepBanner(String s) {
+		stepLabel.setText(s);
+	}
+
+	public void setBorderBanner(String s) {
+		borderLabel.setText(s);
+	}
+
+	public JPanelDraw getPanelDessin() {
+		return panelDraw;
+	}
+
+	public void instantiateSimu() {
+		if (mySimu == null) {
+			mySimu = new Simulator(this);
+			panelDraw.setSimu(mySimu);
+		}
+	}
+
+	public void clicButtonGo() {
+		this.instantiateSimu();
+		if (!mySimu.isAlive()) {
+			mySimu.start();
+		} else {
+			mySimu.togglePause();
+		}
+	}
+
+	public void clicButtonStop() {
+		if (mySimu != null) {
+			panelDraw.setSimu(null);
+			mySimu.stopSimu();
+			mySimu = null;
+			this.eraseLabels();
+			panelDraw.repaint();
+		}
+	}
+
+//	public void clicButtonPause() {
+//		if(mySimu != null) {
+//			mySimu.togglePause();
+//		}
+//	}
+
+	public void clicButtonBorder() {
+		if (mySimu != null) {
+			mySimu.toggleLoopingBorder();
+			borderLabel.setText("border : " + (mySimu.isLoopingBorder() ? "loop" : "closed"));
+		}
+	}
+
+	public void generateRandomBoard() {
+		this.instantiateSimu();
+		float chanceOfLife = ((float) randSlider.getValue()) / ((float) randSlider.getMaximum());
+		mySimu.generateRandom(chanceOfLife);
+		panelDraw.repaint();
+	}
+
+	public void changeSpeed() {
+		if (mySimu != null) {
+			int delay = (int) Math.pow(2, 10 - speedSlider.getValue());
+			mySimu.setLoopDelay(delay);
+		} else {
+			speedSlider.setValue(3);
+		}
+	}
+
+	public void clicLoadFileButton() {
+		Simulator loadedSim = new Simulator(this);
+		String fileName = SelectFile();
+		if (fileName.length() > 0) {
+			try {
+				BufferedReader fileContent = new BufferedReader(new FileReader(fileName));
+				String line = fileContent.readLine();
+				int y = 0;
+				while (line != null) {
+					loadedSim.populateLine(y, line);
+					y++;
+					line = fileContent.readLine();
+				}
+				fileContent.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			if (mySimu != null) {
+				mySimu.stopSimu();
+				this.eraseLabels();
+			}
+			mySimu = loadedSim;
+			panelDraw.setSimu(mySimu);
+			this.repaint();
+		}
+	}
+
+	public void clicSaveToFileButton() {
+		String fileName = SelectFile();
+		if (fileName.length() > 0) {
+			String[] content = mySimu.getFileRepresentation();
+			writeFile(fileName, content);
+		}
+	}
+
+	public String SelectFile() {
+		String s;
+		JFileChooser chooser = new JFileChooser();
+		chooser.setCurrentDirectory(new java.io.File("."));
+		chooser.setDialogTitle("Choose a file");
+		chooser.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
+		chooser.setAcceptAllFileFilterUsed(true);
+		if (chooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION) {
+			s = chooser.getSelectedFile().toString();
+		} else {
+			System.out.println("No Selection ");
+			s = "";
+		}
+		return s;
+	}
+
+	public void writeFile(String fileName, String[] content) {
+		FileWriter csvWriter;
+		try {
+			csvWriter = new FileWriter(fileName);
+			for (String row : content) {
+				csvWriter.append(row);
+				csvWriter.append("\n");
+			}
+			csvWriter.flush();
+			csvWriter.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void update(int stepCount) {
+		this.setStepBanner("Step : " + stepCount);
+		this.repaint();
+	}
+
+	public void eraseLabels() {
+		this.setStepBanner("Step : X");
+		this.setBorderBanner("border : X");
+		speedSlider.setValue(3);
+	}
+
+}
